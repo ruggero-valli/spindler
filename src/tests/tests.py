a63e0997a88682cc51ce10ac983ef083203f598a
Ruggero Valli <ruggerovalli@gmail.com>
2024-01-09 01:10:00 +0100

    trying to make a package

--- src/tests/tests.py
+++ src/tests/tests.py
@@ -1,8 +1,8 @@
 import unittest
 import numpy as np
 
-from spindler.src.solver import Solver_DD21, Solver_Siwek23, Solver_Zrake21
-from spindler.src.interpolator import RegularGridInterpolatorNNextrapol
+from spindler.src.spindler.solver import Solver_DD21, Solver_Siwek23, Solver_Zrake21
+from spindler.src.spindler.interpolator import RegularGridInterpolatorNNextrapol
 
 class TestRegularGridInterpolatorNNextrapol(unittest.TestCase):
 
